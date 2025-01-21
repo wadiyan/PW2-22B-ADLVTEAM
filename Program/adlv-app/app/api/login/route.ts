60dda98f9080f23c1322fa8cf6ca79579e55aebb
master <wadiyan@teknokrat.ac.id>
2025-01-21 09:36:30 +0700

    hafizd: fixing bug mounted route

--- Program/adlv-app/app/api/login/route.ts
+++ Program/adlv-app/app/api/login/route.ts
@@ -1,16 +1,13 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma"; // Inisialisasi Prisma Client
-import bcrypt from "bcryptjs"; // Untuk hashing password
 
 export async function POST(request: Request) {
   const { email, password } = await request.json();
 
   try {
-    // Cari pengguna berdasarkan email
     const user = await prisma.user.findUnique({ where: { email } });
 
-    // Jika pengguna tidak ditemukan atau password tidak cocok
-    if (!user || !(await bcrypt.compare(password, user.password))) {
+    if (!user || user.password !== password) {
       return NextResponse.json(
         { error: "Invalid email or password" },
         { status: 401 }
